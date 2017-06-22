e13e1808862884c0230502854264c5c3207828bc
allen <allen@orientalgame99.com>
2017-06-22 03:00:20 -0400

    3-4 字符串扩展(下)

--- app/js/class/lesson4.js
+++ app/js/class/lesson4.js
@@ -40,3 +40,49 @@
         console.log('es6', code);
     }
 }
+
+{
+    let str = "string";
+    console.log('includes', str.includes("c"));
+    console.log('start', str.startsWith('str'));
+    console.log('end', str.endsWith('ng'));
+}
+
+{
+    //重复输出
+    let str = "abc";
+    console.log(str.repeat(2));
+}
+
+{
+    //模板
+    let name = "list";
+    let info = "hello world";
+    let m = `i am ${name},${info}`;
+    console.log(m);
+}
+
+{
+    //补0
+    console.log('1'.padStart(2, '0'));
+    console.log('1'.padEnd(2, '0'));
+}
+
+{
+    //标签模板
+    let user = {
+        name: 'list',
+        info: 'hello world'
+    };
+    console.log(abc`i am ${user.name},${user.info}`);
+    function abc(s, v1, v2) {
+        console.log(s, v1, v2);
+        return s + v1 + v2
+    }
+}
+
+{
+    //转义
+    console.log(String.raw`Hi\n${1 + 2}`);
+    console.log(`Hi\n${1 + 2}`);
+}
