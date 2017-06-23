64048f5a49352d0ecf8069a94dbd32077fd6b006
allen <allen@orientalgame99.com>
2017-06-22 20:56:06 -0400

    4-3 创建数据计算模块

--- app/js/lottery.js
+++ app/js/lottery.js
@@ -4,5 +4,5 @@
 
 import './lottery/base.js';
 import './lottery/timer.js';
-import './lottery/timer.js';
-import './lottery/timer.js';
+import './lottery/calculate.js';
+import './lottery/interface.js';
--- app/js/lottery/timer.js
+++ app/js/lottery/timer.js
@@ -40,4 +40,6 @@ class Timer {
             }, 1000);
         }
     }
+
+    export default Timer
 }
